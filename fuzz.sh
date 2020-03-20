8fafead32896862ceedc33368bc04c6c1720e41e
Timo Ludwig <ti.ludwig@web.de>
2020-03-21 00:59:35 +0100

    Add fuzzing script

---
+++ fuzz.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# do not send core dumps to external utility
+if [ "$(cat /proc/sys/kernel/core_pattern)" != "core" ]; then
+    echo core | sudo tee /proc/sys/kernel/core_pattern
+fi
+
+# optimize cpu scaling
+if [ "$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)" != "performance" ]; then
+    echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+fi
+
+# activate debug mode
+#export AFL_DEBUG=1
+
+# activate child output
+#export AFL_DEBUG_CHILD_OUTPUT=1
+
+# set afl tmp dir so system tmp dir (which is mounted as tmpfs in ram)
+export AFL_TMPDIR="/tmp"
+
+# remove current input if exists
+if [ -f /$AFL_TMPDIR/.cur_input ]; then
+    rm /$AFL_TMPDIR/.cur_input
+fi
+
+# set afl entrypoint
+export AFL_ENTRYPOINT=0x41d720
+
+# start to fuzz
+./AFLplusplus/afl-fuzz -i in -o out -t 9999999999999999 -m none -d -Q ./jsc fuzz.js
