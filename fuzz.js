797b423330e1067b2203afcefdf821dc74d7ebbd
Timo Ludwig <ti.ludwig@web.de>
2020-06-11 12:17:50 +0200

    Request AFL input data types in JavaScript
    This adds the possiblity to call e.g. getAFLInputArray([bool, integer, double, string]) from a JavaScript file which returns random data from AFL formatted to these types.

--- fuzz.js
+++ fuzz.js
@@ -1,3 +1,4 @@
 print("[JS] getAFLInputArray()");
-let a = getAFLInputArray();
-print('[JS] value of a: ' + a);
+afl_input_types = ['bool', 'integer', 'double', 'string']
+let afl_input_array = getAFLInputArray(afl_input_types);
+print('[JS] AFL input array: ' + afl_input_array);
