9d120108487d2283c21a1aeb238599c85a97d81d
Timo Ludwig <ti.ludwig@web.de>
2020-07-07 01:45:37 +0200

    Fuzz different scripts in infinite loop

--- fuzz.sh
+++ fuzz.sh
@@ -13,38 +13,6 @@ if [[ "$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)" != "perform
     echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 fi
 
-# check if fuzzilli results and enough interesting samples exist
-if [[ ! -d fuzzilli_results/interesting ]] || [[ $(ls -1 fuzzilli_results/interesting | wc -l) < 100 ]]; then
-    # do not send core dumps to external utility
-    if [[ "$(cat /proc/sys/kernel/core_pattern)" != "|/bin/false" ]]; then
-        sudo sysctl -w 'kernel.core_pattern=|/bin/false' > /dev/null
-    fi
-    # generate interesting js-scripts
-    cd fuzzilli
-    IMPORT_STATE=$(if [[ -f ../fuzzilli_results/state.json ]]; then printf "--importState=../fuzzilli_results/state.json"; fi)
-    swift run FuzzilliCli --numIterations=20 --exportState --exportStatistics --storagePath=../fuzzilli_results ${IMPORT_STATE} --logLevel=verbose --profile=jsc ../jsc_fuzzilli
-    cd ..
-fi
-
-# do not send core dumps to external utility
-if [[ "$(cat /proc/sys/kernel/core_pattern)" != "core" ]]; then
-    sudo sysctl -w 'kernel.core_pattern=core' > /dev/null
-fi
-
-# activate debug mode
-export AFL_DEBUG=1
-
-# activate child output
-export AFL_DEBUG_CHILD_OUTPUT=1
-
-# set afl tmp dir so system tmp dir (which is mounted as tmpfs in ram)
-export AFL_TMPDIR="/tmp"
-
-# remove current input if exists
-if [[ -f /${AFL_TMPDIR}/.cur_input ]]; then
-    rm /${AFL_TMPDIR}/.cur_input
-fi
-
 # set afl entrypoint
 if [[ -f .afl_entrypoint ]]; then
     export AFL_ENTRYPOINT=$(cat .afl_entrypoint)
@@ -60,26 +28,69 @@ if [[ ! -d afl_input ]]; then
     mkdir afl_input
 fi
 
-# find interesting sample to fuzz
-mapfile -t JS_FILES < <( find fuzzilli_results/interesting -type f -name "*.js" )
+# force english locale to make output of diff command comparable
+export LANG=en_US.utf8
 
-# iterate array
-for JS_FILE in "${JS_FILES[@]}"; do
-    # pick first file which was not yet fuzzed
-    JS_FILENAME=$(basename ${JS_FILE})
-	if [[ ! -f target_scripts/${JS_FILENAME} ]]; then
-        break
-	fi
+# activate debug mode
+#export AFL_DEBUG=1
+
+# activate child output
+#export AFL_DEBUG_CHILD_OUTPUT=1
+
+# set afl tmp dir so system tmp dir (which is mounted as tmpfs in ram)
+export AFL_TMPDIR="/tmp"
+
+# infinite fuzzing loop
+while true; do
+
+    # check if fuzzilli results exist and if they contain interesting samples which are not yet in target scripts
+    if [[ ! -d fuzzilli_results/interesting ]] || ! diff -q fuzzilli_results/interesting target_scripts | grep -q "Only in fuzzilli_results/interesting:"; then
+        # do not send core dumps to external utility
+        if [[ "$(cat /proc/sys/kernel/core_pattern)" != "|/bin/false" ]]; then
+            sudo sysctl -w 'kernel.core_pattern=|/bin/false' > /dev/null
+        fi
+        # generate interesting js-scripts
+        cd fuzzilli
+        IMPORT_STATE=$(if [[ -f ../fuzzilli_results/state.json ]]; then printf "--importState=../fuzzilli_results/state.json"; fi)
+        swift run FuzzilliCli --numIterations=20 --exportState --exportStatistics --storagePath=../fuzzilli_results ${IMPORT_STATE} --logLevel=verbose --profile=jsc ../jsc_fuzzilli
+        cd ..
+    fi
+
+    # find interesting sample to fuzz
+    mapfile -t JS_FILES < <( find fuzzilli_results/interesting -type f -name "*.js" )
+    # iterate array
+    for JS_FILE in "${JS_FILES[@]}"; do
+        # pick first file which was not yet fuzzed
+        JS_FILENAME=$(basename ${JS_FILE})
+        if [[ ! -f target_scripts/${JS_FILENAME} ]]; then
+            break
+        fi
+    done
+
+    # convert js file
+    ./js_converter.py ${JS_FILE} -o target_scripts
+
+    # get desired afl input size
+    AFL_INPUT_SIZE=$(python -c "import json; file = open('.afl_input_sizes.json'); data = json.load(file); print(data['target_scripts/${JS_FILENAME}'])")
+
+    # write test case of desired size
+    dd if=/dev/urandom of=afl_input/afl_input_seed bs=1 count=${AFL_INPUT_SIZE} > /dev/null
+
+    # do not send core dumps to external utility
+    if [[ "$(cat /proc/sys/kernel/core_pattern)" != "core" ]]; then
+        sudo sysctl -w 'kernel.core_pattern=core' > /dev/null
+    fi
+
+    # remove current input if exists
+    if [[ -f /${AFL_TMPDIR}/.cur_input ]]; then
+        rm /${AFL_TMPDIR}/.cur_input
+    fi
+
+    # start to fuzz
+    ./AFLplusplus/afl-fuzz -i afl_input -o afl_results -t 9999999999999999 -m none -d -V 10000 -Q ./jsc_afl target_scripts/${JS_FILENAME}
+
+    # enable aborting between runs ([CTRL + C] only aborts AFL)
+    echo "If you want to abort, please press [CTRL + C] now."
+    sleep 3
+
 done
-
-# convert js file
-./js_converter.py ${JS_FILE} -o target_scripts
-
-# get desired afl input size
-AFL_INPUT_SIZE=$(python -c "import json; file = open('.afl_input_sizes.json'); data = json.load(file); print(data['target_scripts/${JS_FILENAME}'])")
-
-# write test case of desired size
-dd if=/dev/urandom of=afl_input/afl_input_seed bs=1 count=${AFL_INPUT_SIZE} > /dev/null
-
-# start to fuzz
-./AFLplusplus/afl-fuzz -i afl_input -o afl_results -t 9999999999999999 -m none -d -Q ./jsc_afl target_scripts/${JS_FILENAME}
